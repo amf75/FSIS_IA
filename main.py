f7be39767a8f8a6f9582dd121af4bb4d26d7cb6a
amf75 <am.flacco@gmail.com>
2025-02-13 15:12:40 +0100

    ok

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  
 import pandas as pd
 import tensorflow as tf
-from tensorflow import keras
+import keras
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
@@ -13,3 +13,34 @@ breast_cancer = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-dat
 
 breast_cancer.head()
 print(breast_cancer.head())
+X = breast_cancer.drop(['diagnosis','id'],axis=1).values
+Y = breast_cancer['diagnosis'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.3, random_state=0)
+le = LabelEncoder()
+y_train = le.fit_transform(y_train)
+y_test = le.transform(y_test)
+ss = StandardScaler()
+X_train = ss.fit_transform(X_train)
+X_test = ss.transform(X_test)
+X_train.shape
+print(X_train.shape)
+model =  keras.models.Sequential()
+model.add(keras.layers.Dense(12, input_dim=X_train.shape[1], activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
+model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(X_train, y_train, epochs=100)
+loss, acc = model.evaluate(X_test, y_test)
+print("Loss sul test set: %.4f" % loss)
+print("Accuracy sul test set: %.4f" % acc)
+model = keras.models.Sequential()
+
+model.add(keras.layers.Dense(12, input_dim=X_train.shape[1], activation="relu"))
+model.add(keras.layers.Dense(8, activation="relu"))
+model.add(keras.layers.Dense(4, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(X_train, y_train, epochs=100)
